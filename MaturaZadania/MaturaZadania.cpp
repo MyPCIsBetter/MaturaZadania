78f686f5520b197322c9f0d36f4c141dd530bf04
Piotrek <piotrekk1@outlook.com>
2016-06-03 10:39:45 +0200

    Dodano Puchar Świata zadanie 5.1

--- MaturaZadania/MaturaZadania.cpp
+++ MaturaZadania/MaturaZadania.cpp
@@ -2,10 +2,96 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <direct.h>
+using namespace std;
 
+vector<string> splitString(string text, char delimiter) {
+	vector<string> result;
+	string wordToAdd = "";
+
+	for (int i = 0; i < text.size(); i++) {
+
+		if (text[i] == delimiter) {
+			result.push_back(wordToAdd);
+			wordToAdd = "";
+		}
+		else {
+			wordToAdd += text[i];
+		}
+
+	}
+
+	if (wordToAdd != ""){
+		result.push_back(wordToAdd);
+	}
+
+	return result;
+}
 
 int main()
 {
-    return 0;
+	map<string, int> countries; //panstwo, ilosc zwyciêzców
+
+	string pathToDirectory = _getcwd(NULL, 0);
+
+	string pathToPucharFile = pathToDirectory + "\\Dane\\puchar.txt";
+	string pathToZawodnicyFile = pathToDirectory + "\\Dane\\zawodnicy.txt";
+
+	ifstream filePuchar(pathToPucharFile);
+	if (!filePuchar) {
+		cout << "Problem z odczytaniem pliku";
+	}
+	else {
+		cout << "Ok";
+		string line;
+		bool omitFirstLine = true;
+
+		while (getline(filePuchar, line)) {
+			if (omitFirstLine) { 
+				omitFirstLine = false;
+				continue; 
+			}
+
+			vector<string> oneLinePucharData = splitString(line, ';');
+
+			string position = oneLinePucharData[1];
+			string winnerId = oneLinePucharData[2];
+
+			if (position == "1") {
+				ifstream fileZawodnicy(pathToZawodnicyFile);
+				if (fileZawodnicy) {
+					string playersLine;
+					bool omitFirstLine2 = true;
+					while (getline(fileZawodnicy, playersLine)) {
+						if (omitFirstLine2) {
+							omitFirstLine2 = false;
+							continue;
+						}
+
+						vector<string> oneLineZawodnicyData = splitString(playersLine, ';');
+
+						string playerId = oneLineZawodnicyData[0];
+						string panstwo = oneLineZawodnicyData[2];
+
+						if (winnerId == playerId) {
+							countries[panstwo] ++;
+							break;
+						}
+					}
+				}
+				else {
+					cout << "Problem z odczytem zawodnikow";
+				}
+			}
+		}
+		filePuchar.close();
+	}
+
+	return 0;
 }
 
